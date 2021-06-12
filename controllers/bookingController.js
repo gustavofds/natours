6313fc324873997dced4feb8f419748f7969da2c
Gustavo Sousa <gustavohl98@gmail.com>
2021-06-12 19:54:42 -0300

    Fixed error with stripe - price = amount_total

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,8 +55,8 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   console.log(session);
-  // const price = session.display_items[0].amount / 100;
-  await Booking.create({ tour, user /* price */ });
+  const price = session.amount_total / 100;
+  await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
