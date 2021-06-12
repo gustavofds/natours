b599e0295705e877e96b73964e63f0dd03ba963d
Gustavo Sousa <gustavohl98@gmail.com>
2021-06-12 19:55:20 -0300

    Remove unecessary console.log

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,7 +54,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  console.log(session);
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
